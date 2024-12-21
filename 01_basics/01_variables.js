8b2ce9f1235614b1fea8fa32e5467fe1f02264c7
Rajdeep Sanjay Parbhane <102216713+Ratz21@users.noreply.github.com>
2024-12-21 09:01:38 +0000

    datatypes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -11,7 +11,7 @@ accountEmail = "Rj@rc.com"
 accountPassword = "19191919"
 accountCity = "Pune"
 accountState = "Maharashtra"
-console.log(accountId); // this was only to print on section thru to table we can print every variable
+console.log(accountId) // this was only to print on section thru to table we can print every variable by console.table
 /* prefer not to use var bcz of block scope issue use let */
 
 console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict" // treat all JS code as newer version as it uses modules & class 
+
+// alert in (3+3) gave us directly output in browser inspect but as we know the engine was present for exection in js its seperated 
+
+console.log(3+3) 
+console.log("Rajdeep"); // always print in new line 
+
+let name = "Raj"
+let age = 21
+let isLoggedin = true
+
+/*number = 2^53 
+  bigint = used for redit or Trading view
+  string = ""
+  boolean = true/false
+  null = standalone value / object
+  undefined = undefined
+  symbol =  to find unqiuesness in a component from figma 
+*/
+console.log(typeof "Raj");
+console.log(typeof true);
+console.log(typeof 21);
+console.log(typeof null); //null is object where as undefined is type
+
+
+
+
+
